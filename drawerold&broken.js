026ae0b938ceb71be3a8264fcbd87ebf937eb8d2
spunkx <spunkx+tspinks@our.ecu.edu.au>
2020-05-28 22:54:58 +0800

    old tictactoe approach
    I was getting hungup on the logic for getting canvas working with multiple layers, rather than working on getting tictactoe working

---
+++ drawerold&broken.js
@@ -0,0 +1,72 @@
+
+//ordinarily https://www.w3schools.com/graphics/canvas_drawing.asp
+
+//lineLocationX & lineLocationY = where you want my line on the screen
+//https://www.w3resource.com/html5-canvas/html5-canvas-lines.php
+function drawLine(start, end, selector, direction){
+    //selector = boxy.querySelector(selector);
+
+    let ctx = selector.getContext("2d");
+    //for now direction is a string, but later will change to a degrees or radians measurement
+    if(direction === "hoz"){
+        //need to use the ACTUAL locations to define the x and y, literally, rather than using an abstract value
+        //ctx.strokeStyle = "#00ff00";
+        ctx.beginPath();
+        ctx.moveTo(0, start);
+        ctx.lineTo(end, start);
+        ctx.strokeStyle = "green";
+        ctx.stroke();
+    }
+    else if(direction === "vert"){
+        ctx.beginPath();
+        ctx.moveTo(start, 0);
+        ctx.lineTo(start, end);
+        ctx.strokeStyle = "green";
+        ctx.stroke();
+    }
+    //tictactoe cross
+    else if(direction === "posdiag"){
+        ctx.beginPath();
+        ctx.moveTo(start, 0);
+        ctx.lineTo(start, end);
+        ctx.strokeStyle = "red";
+        ctx.stroke;
+        //draw from bottom corner to top corner + and - a few pixels
+
+    }
+    else if(direction === "negdiag"){
+        ctx.beginPath();
+        ctx.moveTo(0, start);
+        ctx.lineTo(end, start);
+        ctx.strokeStyle = "red";
+        ctx.stroke;
+        //same as above but opposite corners
+    }
+    //must invert lineLength with x
+
+}
+
+//tictactoe naught
+function drawCurve(x, y, size, selector){
+    let ctx = selector.getContext("2d"); 
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.arc(x, y, size, 0, Math.PI * 2, false);
+    ctx.stoke;
+}
+/*
+function eraseLine(){
+
+}*/
+
+/*
+create shape of n vertices
+//see note book for a working theory on even vertices 
+
+function createTriangle(){
+
+}
+
+function createCircle(){
+
+}*/
